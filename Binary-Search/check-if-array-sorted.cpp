94a9292966a3afbe2e21c5426455240e28f00cd6
Devang2304 <devangvartak2304@gmail.com>
2022-11-13 16:08:19 +0530

    Binary search technique

--- Binary-Search/check-if-array-sorted.cpp
+++ Binary-Search/check-if-array-sorted.cpp
@@ -41,3 +41,56 @@ int main() {
     }
     return 0;
 }
+
+// method two using binary search algorithm
+// bc esko TLE dikha raha hai binary search se kiya toh
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        int s=0,count=1;
+        int e=n-1;
+        int mid=s+(e-s)/2;
+        while(s<e){
+            
+            if(n==1){
+                return 1;
+            }
+            else if(arr[mid]>=arr[0]){
+                s=mid+1;
+            }
+            else{
+                count=0;
+                e=mid;
+            }
+            mid=s+(e-s)/2;
+        }
+        if(count==1){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        bool ans = ob.arraySortedOrNot(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
