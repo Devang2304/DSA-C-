e6874529fcaa6ace11d987d744373f1396dbd043
Devang2304 <devangvartak2304@gmail.com>
2022-12-24 13:41:10 +0530

    Bohot wrong submissions ke baad sahi aya hai

--- codeforce-Qs/sortZero.cpp
+++ codeforce-Qs/sortZero.cpp
@@ -1,25 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
+int dev[100005];
+int main()
+{
+    int t; cin>>t;
     while(t--){
-        int n;
-        int cnt=0;
-        cin>>n;
-        int arr[n];
-        for(int i=0; i<n; i++) cin>>arr[i];
-        for (int i = 0; i < n-1; i++)
-        {
-            for (int j =0; j < n-i-1; j++)
-            {
-                if(arr[j]>arr[j+1])
-                swap(arr[j],arr[j+1]);
-                cnt++;
-            }
-            
+        int i, n, last=-1, ans=0; cin>>n;
+        int a[n];
+        for(i=0; i<n; i++){
+            cin>>a[i];
         }
-        cout<<cnt<<endl;
+        for(i=n-1; i>0; i--){
+            if(a[i-1] > a[i]){
+              last=i-1;
+              break;
+            }
+        }
+        if(last>=0){
+        for(i=0; i<=last; i++){
+                dev[a[i]]=1;
+        }
+        for(i=last+1; i<n; i++){
+            if(dev[a[i]] == 1){
+               last=i;
+            }
+        }
+        for(i=0; i<=last; i++){
+                dev[a[i]]=1;
+        }
+        for(i=0; i<=last; i++){
+            if(dev[a[i]]>0)
+            ans+=dev[a[i]];
+            dev[a[i]]--;
+        }
+        for(i=1; i<100005; i++){
+            dev[i]=0;
+        }
     }
-    return 0;
+        cout<<ans<<endl;
+    }
 }
