2e55aa6e4cf6ca282d19ac9626048249a7b75793
Devang2304 <devangvartak2304@gmail.com>
2022-12-29 14:20:02 +0530

    Solved one Q

--- codeforce-Qs/A-Sum-of-Round-Numbers.cpp
+++ codeforce-Qs/A-Sum-of-Round-Numbers.cpp
@@ -1,32 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int printZero(int x){
-    while(x--){
+typedef long long ll;
+
+void printZero(int x)
+{
+    while (x--)
         cout << 0;
-    }
 }
-int main(){
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
-    string n;
-    cin>>n;
-    int k=0;
-    for(int i=0; i<n.size(); i++)
-        if(n[i]!='0')
-            k++;
-    cout<<k<<"\n";
-    for(int i=0; i<n.size(); i++){
-        if(n[i]!='0'){
-            cout<<n[i];
-            printZero((n.size()-i-1));
-            cout<<" ";
-        }        
+        string n;
+        cin >> n;
+        int k = 0;
+        for (int i = 0; i < n.size(); i++)
+            if (n[i] != '0')
+                k++;
+        cout << k << "\n";
+        for (int i = 0; i < n.size(); i++)
+        {
+            if (n[i] != '0')
+            {
+                cout << n[i];
+                printZero(n.size() - i - 1);
+                cout << " ";
+            }
+        }
+        cout << endl;
     }
-    cout<<endl;
-}
-    
     return 0;
 }
