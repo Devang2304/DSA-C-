e34f3be6df0be9a3b86f4f896795c30d8ee5d5d9
Devang2304 <devangvartak2304@gmail.com>
2022-12-29 18:06:11 +0530

    Done using Binary Search observation accha hoga toh easily ho gayega

--- Binary-Search/Rotation.cpp
+++ Binary-Search/Rotation.cpp
@@ -38,3 +38,55 @@ int main() {
     }
     return 0;
 }
+
+//using binary search approach 
+// observation jaruri h dont just go with naive approach
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) {
+	    int low=0;
+	    int high=n-1;
+	    int mid=0;
+	    while(low<high){
+	          mid=(low+high)>>1;
+	        if(arr[mid]<arr[high]){
+	            high=mid;
+	        }
+	        else{
+	            low=mid+1;
+	        }
+	    }
+	    return low;
+	}
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findKRotation(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
